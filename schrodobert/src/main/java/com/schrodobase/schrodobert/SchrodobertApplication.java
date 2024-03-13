dc9a033e1246c59623b9c8abd855b2b87a87fc0b
goldenshiber <johan_soodla@hotmail.com>
2024-03-13 11:37:46 +0100

    Updated with yaml test, URL WIP

--- schrodobert/src/main/java/com/schrodobase/schrodobert/SchrodobertApplication.java
+++ schrodobert/src/main/java/com/schrodobase/schrodobert/SchrodobertApplication.java
@@ -1,13 +1,26 @@
 package com.schrodobase.schrodobert;
 
+import com.schrodobase.schrodobert.config.YAMLConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchrodobertApplication {
+public class SchrodobertApplication implements CommandLineRunner {
 
+	@Autowired
+	private YAMLConfig myConfig;
 	public static void main(String[] args) {
-		SpringApplication.run(SchrodobertApplication.class, args);
+		SpringApplication app = new SpringApplication(SchrodobertApplication.class);
+		app.run();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("using environment: " + myConfig.getEnvironment());
+		System.out.println("name: " + myConfig.getName());
+		System.out.println("enabled:" + myConfig.isEnabled());
+		System.out.println("servers: " + myConfig.getServers());
+	}
 }
